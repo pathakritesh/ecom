ef4b740851736fd045c09eb9f7c175d70cd4d4d8
unknown <ritesh>
2020-11-11 17:57:31 +0530

    template

---
+++ js/custom.js
@@ -0,0 +1,47 @@
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+$(function(){
+    var navMain = $(".navbar-collapse");
+
+    navMain.on("click", "a", null, function () {
+        navMain.collapse('hide');
+    });
+});
+
+
+  // header fixed on scroll
+  $(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $("header").addClass("active");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $("header").removeClass("active");
+    }
+});
+
+    // animation aos
+   $(document).ready(function ($) {
+    if (screen.width > 1024) {
+        AOS.init({
+            easing: 'ease-in-out-sine',
+            once: true,
+        });
+    }
+});
